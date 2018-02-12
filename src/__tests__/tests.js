333155ad51f361e160a4dd1bc133c7a67b3b8fa7
Dylan Cunningham <gdylanc@gmail.com>
2018-02-12 16:34:47 -0700

    adds shallow rendering of app test

--- src/__tests__/tests.js
+++ src/__tests__/tests.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../App';
+import InputValidation from '../components/InputValidation';
+import { shallow } from 'enzyme';
 
 
-describe('InputValidators', () => {
+describe('InputValidation', () => {
 
-    it('renders without crashing', () => {
+    it('smokescreen test for shallow render of App', () => {
+        shallow(<App />);
+    });
+
+    it('smokescreen test for deep render of App', () => {
         const div = document.createElement('div');
         ReactDOM.render(<App />, div);
     });
 
 
 
-})
+});
+
+
+// ReactDom.render(<InputValidation />, div)
